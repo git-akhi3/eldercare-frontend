49e202256cbb6cc8843e42443ac791e63c487a40
git-akhi3 <akhilmulagada20@gmail.com>
2024-10-26 18:34:08 +0530

    fixed performance

--- iithyd/tailwind.config.js
+++ iithyd/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",  // This will include all js, ts, jsx, tsx files in src directory
-    "./components/**/*.{js,ts,jsx,tsx}",  // If you have a components directory at root level
-    "./app/**/*.{js,ts,jsx,tsx}",  // If you have an app directory
-    "./**/*.{js,ts,jsx,tsx}",],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+    "./components/**/*.jsx",  // Changed this line
+    "./app/**/*.{js,jsx}",
+  ],
   theme: {
     extend: {},
   },
